54901e5251e4d476d0c6eef4bf586f0b9601a130
Saptarshi <saimndl.online.007@gmail.com>
2022-08-16 13:08:46 +0530

    changes

--- S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
+++ S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
@@ -1,13 +1,21 @@
 package com.samples.S01springcoredi;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       ClassPathXmlApplicationContext springContainer = new ClassPathXmlApplicationContext("com/samples/S01springcoredi/SpringConfig.xml");
+       
+//       Employee emp = (Employee) springContainer.getBean("emp"); 
+//     System.out.println(emp);
+
+//       Employee em1 = new Employee();
+//       em1.setId(10);
+//       em1.setName("x");
+       
+       Hospital hospital = (Hospital) springContainer.getBean("hospital");
+       System.out.println(hospital);
     }
 }
